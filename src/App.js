4894ab6a755c57fd0357e44094b9d0c2e9a0a8d0
miguelmoranv <miguelonchas001@gmail.com>
2023-01-20 14:13:28 -0600

    P2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
-function App() {
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <h2>Miguel Angel Moran Vazquez 5B-TI</h2>
+      <h4>Universidad Tecnologica de Durango</h4>
+      <Button as="a" href="https://utd.edu.mx">UTD</Button>
+      <Button as="a" href="https://github.com/miguelmoranv" primary>GitHub</Button>
+      <Button as="a" href="https://www.facebook.com/miguelangel.moranvazques">Facebook</Button>
     </div>
   );
 }
-
-export default App;
